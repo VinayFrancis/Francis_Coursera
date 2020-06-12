4016c32a73f0cd644433662a53a71135b32dc0d1
Vinay <vinayf84@gmail.com>
2020-06-13 00:20:02 +0530

    Third commit

---
+++ stats.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include "stats.h"
+#define SIZE (40)
+float avg1(unsigned char *p,int size);
+float med(unsigned char *p,int size);
+int min(unsigned char *p, int size);
+int max(unsigned char *p, int size);
+void printarr(unsigned char *p,int size);
+void srtarr(unsigned char *p,int size);
+void printstats();
+float a,b;
+int c,d;
+void main() {
+
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+
+
+     a = avg1(test,SIZE);
+     b = med(test,SIZE);
+     c = max(test,SIZE);
+     d = min(test,SIZE);
+     printstats();
+     srtarr(test,SIZE);
+     printarr(test,SIZE);
+
+
+}
+
+void printstats(){
+
+    	printf("Average =%f",a);
+
+    	printf("\nMedian = %f", b);
+
+    	printf("\nMaximum = %d", c);
+
+    	printf("\nMinimum = %d",d);
+
+}
+
+float avg1(unsigned char *p,int size){
+	int sum = 0;
+	float average;
+
+	for(int i =0;i<size;i++){
+		sum +=*p;
+		p++;
+	}
+	average = sum/size;
+	return average;
+}
+
+float med(unsigned char *p,int size){
+	int ipos;
+	int fpos;
+	float median;
+	int i;
+	if(size%2==0){
+		int s=0;
+		ipos =size/2;
+	  fpos =ipos +1;
+	  for(i =1;i<fpos+1;i++){
+	    	if(i==ipos){
+	    		s += *p;
+			    p++;
+		    }
+		    else if(i==fpos){
+		    	s += *p;
+			    p++;
+		    }
+		    else{
+		    	p++;
+		    }
+    }
+	    median = s/2;
+	    return median;
+  }
+	else{
+		ipos= (size+1)/2;
+		for(i = 1; i<=ipos;i++){
+			if(i==ipos){
+				median =*p;
+			}
+			p++;
+		}
+		return median;
+	}
+}
+
+int max(unsigned char *p, int size){
+	int max = 0;
+	for(int i =0;i<size;i++){
+		if(max==0){
+			max = *p;
+			p++;
+		}
+		else if ((*p)>max){
+			max =*p;
+			p++;
+		}
+		else{
+			p++;
+		}
+	}
+	return max;
+}
+
+int min(unsigned char *p, int size){
+	int min = 0;
+	for(int i =0;i<size;i++){
+		if(min==0){
+			min = *p;
+
+		}
+		else if ((*p)<min){
+			min=*p;
+
+		}
+		p++;
+	}
+	return min;
+}
+
+void srtarr(unsigned char *s,int size){
+	int temp;
+	unsigned char *p;
+	for(int a =0;a<size-1;a++){
+		p=s;
+		for (int i =0 ; i<size-a-1; i++){
+			if(*p<*(p+1)){
+				temp = *p;
+			    *p = (*(p+1));
+			    *(p+1)=temp;
+
+		    }
+		    p++;
+	    }
+
+	}
+
+}
+void printarr(unsigned char *p,int size){
+	printf("\n");
+	for (int i =0;i<size;i++){
+		printf("%d\n",*p);
+		p++;
+
+	}
+}
